e9aceda0749ad08ece001791957ebc0886634d7f
Skander <besbes.skander@gmail.com>
2021-05-28 10:24:18 +0200

    0 numerator error

--- src/app/models/fraction.spec.ts
+++ src/app/models/fraction.spec.ts
@@ -4,4 +4,8 @@ describe('Fraction', () => {
   it('should create an instance', () => {
     expect(new Fraction(1, 1)).toBeTruthy();
   });
+
+  it('should not accept 0 as numerator', () => {
+    expect(() => new Fraction(0, 1)).toThrow(Error('invalid numerator'));
+  });
 });
--- src/app/models/fraction.ts
+++ src/app/models/fraction.ts
@@ -3,6 +3,9 @@ export class Fraction {
   private denominator: number;
 
   constructor(numerator: number, denominator: number) {
+    if(numerator === 0) {
+      throw Error('invalid numerator');
+    }
     this.numerator = numerator;
     this.denominator = denominator;
   }
