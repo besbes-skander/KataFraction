b19890c6bfbbbacf6a98d75f2f891e2ba83035d1
Skander <besbes.skander@gmail.com>
2021-05-28 10:43:11 +0200

    spell check

--- src/app/models/fraction.ts
+++ src/app/models/fraction.ts
@@ -24,8 +24,8 @@ export class Fraction {
 
   static add(fraction1: Fraction, fraction2: Fraction): Fraction {
     const numerator = (fraction1.numerator * fraction2.denominator) + (fraction2.numerator * fraction1.denominator)
-    const denumerator = fraction1.denominator * fraction2.denominator;
+    const denominator = fraction1.denominator * fraction2.denominator;
     
-    return new Fraction(numerator, denumerator);
+    return new Fraction(numerator, denominator);
   }
 }
